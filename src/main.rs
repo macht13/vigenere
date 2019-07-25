9efc5e56664f5c60e3bf7fda4e369274a18af491
Sebastian Steiner <e11777731@student.tuwien.ac.at>
2019-07-25 22:47:03 +0200

    test

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,64 @@
-#[macro_use]
-extern crate text_io;
+extern crate rand;
+
+use rand::Rng;
+use std::io;
+use std::time::Instant;
 
 fn main() {
-    println!("Do you want to (E)ncrypt or (D)ecrypt? [E/D]");
-    let mode: String = read!("{}\n");
-    match mode
-        .replace("\r", "")
-        .to_uppercase()
-        .chars()
-        .next()
-        .expect("Invalid input supplied")
-    {
-        'E' => encoder(),
-        'D' => decoder(),
-        _ => println!("Invalid! Please enter either E or D"),
-    };
+    let mut min = String::new();
+    let mut max = String::new();
+    let mut cnt = String::new();
+    println!("Minimum: ");
+    io::stdin()
+        .read_line(&mut min)
+        .expect("Failed to read line");
+    let min: u32 = min.trim().parse().expect("Failed to parse");
+    println!("Maximum (nicht inklusive): ");
+    io::stdin()
+        .read_line(&mut max)
+        .expect("Failed to read line");
+    let max: u32 = max.trim().parse().expect("Failed to parse");
+    println!("Anzahl: ");
+    io::stdin()
+        .read_line(&mut cnt)
+        .expect("Failed to read line");
+    let cnt: u32 = cnt.trim().parse().expect("Failed to parse");
+
+    game(min, max, cnt);
 }
 
-fn encoder() {
-    // get input
-    println!("Please enter plaintext:");
-    let mut plain: String = read!("{}\n");
-    plain = plain.replace("\r", "").replace(" ", "").to_uppercase();
-    println!("Now please enter any number of keys as single lines:");
-    // get key and encrypt
-    loop {
-        // read key
-        let mut key: String = read!("{}\n");
-        key = key.replace("\r", "").replace(" ", "").to_uppercase();
+fn game(min: u32, max: u32, rounds: u32) {
+    let mut times = Vec::new();
 
-        let mut enciphered: Vec<u8> = Vec::new();
-        // iterate over each character of plain text and key (cyclic)
-        for (a, b) in plain.bytes().zip(key.bytes().cycle()) {
-            // compute to decoded value and add it to the vector
-            enciphered.push(encode(a, b));
+    for _ in 0..rounds {
+        let val1 = rand::thread_rng().gen_range(min, max);
+        let val2 = rand::thread_rng().gen_range(min, max);
+        let res = val1 * val2;
+        let mut guess = String::new();
+        println!("{}*{}=", val1, val2);
+        let start = Instant::now();
+        loop {
+            io::stdin()
+                .read_line(&mut guess)
+                .expect("Failed to read guess");
+            let guess: u32 = guess.trim().parse().expect("Failed to parse guess");
+
+            if guess == res {
+                break;
+            }
         }
-        println!("Cipher: {}", std::str::from_utf8(&enciphered).unwrap());
+        times.push(start.elapsed().as_millis());
     }
-}
 
-fn decoder() {
-    // get input
-    println!("Please enter ciphertext:");
-    let mut cipher: String = read!("{}\n");
-    cipher = cipher.replace("\r", "").replace(" ", "").to_uppercase();
-    println!("Now please enter any number of keys as single lines:");
-    // get key and decrypt
-    loop {
-        // read key
-        let mut key: String = read!("{}\n");
-        key = key.replace("\r", "").replace(" ", "").to_uppercase();
+    let times: Vec<f64> = times.iter().cloned().map(|x| x as f64 / 1000.0).collect();
 
-        let mut deciphered: Vec<u8> = Vec::new();
-        // iterate over each character of cipher text and key (cyclic)
-        for (a, b) in cipher.bytes().zip(key.bytes().cycle()) {
-            // compute to decoded value and add it to the vector
-            deciphered.push(decode(a, b));
-        }
-        println!("Plain: {}", std::str::from_utf8(&deciphered).unwrap());
-    }
-}
-
-fn decode(cipher: u8, key: u8) -> u8 {
-    let alpha = "A".as_bytes()[0];
-    (26         // add 26 to avoid underflow
-    + cipher    // the original encrypted character
-    - key       // subtract the key character
-    - 1)        // subtract 1 because a=1 not 0
-        % 26    // wrap around to remain in [0,25]
-        + alpha // add "A" to get back into ascii [65,90]
-}
-
-fn encode(plain: u8, key: u8) -> u8 {
-    let alpha = "A".as_bytes()[0];
-    (plain          // the original char to encrypt
-     + key          // add the key
-     - 2 * alpha    // subtract to get in remove ascii part
-     + 1)           // add 1 because a=1 not 0
-    % 26            // wrap around to remain in [0,25]
-    + alpha // add "A" to get back in ascii [65,90]
+    println!(
+        "Fertig!\n
+             μ: {}s\n
+             max: {}s\n
+             min: {}s",
+        times.iter().cloned().fold(0.0, |a, b| { a + b }) / rounds as f64,
+        times.iter().cloned().fold(0. / 0., f64::max),
+        times.iter().cloned().fold(1. / 0., f64::min)
+    );
 }
