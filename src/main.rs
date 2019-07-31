e86ad36275e2b7bddf83175c1cbf3dac83334079
Sebastian Steiner <e11777731@student.tuwien.ac.at>
2019-07-31 21:44:30 +0200

    other project

--- src/main.rs
+++ src/main.rs
@@ -4,62 +4,93 @@ use rand::Rng;
 use std::io;
 use std::time::Instant;
 
+struct StatData {
+    times: Vec<u128>,
+    tries: Vec<u32>
+}
+
+impl StatData {
+    fn print(&self) {
+    let times: Vec<f64> = self.times.iter().cloned().map(|x| x as f64 / 1000.0).collect();
+
+    println!(
+        "Fertig!\nZeitinformation\n
+        μ: {}s\n
+        max: {}s\n
+        min: {}s",
+        mean_f64(&times),
+        times.iter().cloned().fold(0. / 0., f64::max),
+        times.iter().cloned().fold(1. / 0., f64::min)
+    );
+    println!("Versuchsinformation\n
+            μ: {}\n
+            max: {}\n
+            min: {}\n",
+            mean_u32(&self.tries),
+            self.tries.iter().cloned().fold(u32::min_value(), u32::max),
+            self.tries.iter().cloned().fold(u32::max_value(), u32::min));
+    }
+}
+
 fn main() {
-    let mut min = String::new();
-    let mut max = String::new();
-    let mut cnt = String::new();
     println!("Minimum (oder 10): ");
-    io::stdin()
-        .read_line(&mut min)
-        .expect("Failed to read line");
-    let min: u32 = min.trim().parse().unwrap_or(10);
+    let min = read_num(Some(10));
     println!("Maximum (nicht inklusive oder 100): ");
-    io::stdin()
-        .read_line(&mut max)
-        .expect("Failed to read line");
-    let max: u32 = max.trim().parse().unwrap_or(100);
+    let max = read_num(Some(100));
     println!("Anzahl (oder 5): ");
-    io::stdin()
-        .read_line(&mut cnt)
-        .expect("Failed to read line");
-    let cnt: u32 = cnt.trim().parse().unwrap_or(5);
+    let cnt = read_num(Some(5));
 
     game(min, max, cnt);
 }
 
 fn game(min: u32, max: u32, rounds: u32) {
-    let mut times = Vec::new();
-
+    let mut stats = StatData {
+        times: Vec::new(),
+        tries: Vec::new(),
+    };
     for _ in 0..rounds {
         let val1 = rand::thread_rng().gen_range(min, max);
         let val2 = rand::thread_rng().gen_range(min, max);
         let res = val1 * val2;
-        let mut guess;
         println!("{}*{}=", val1, val2);
         let start = Instant::now();
+        let mut tries = 0;
         loop {
-            guess = String::new();
-            io::stdin()
-                .read_line(&mut guess)
-                .expect("Failed to read guess");
-            let guess: u32 = guess.trim().parse().unwrap_or(0);
-
-            if guess == res {
+            tries += 1;
+            if read_num(None) == res {
                 break;
             }
         }
-        times.push(start.elapsed().as_millis());
+        stats.tries.push(tries);
+        stats.times.push(start.elapsed().as_millis());
     }
 
-    let times: Vec<f64> = times.iter().cloned().map(|x| x as f64 / 1000.0).collect();
+    stats.print();
+}
 
-    println!(
-        "Fertig!\n
-             μ: {}s\n
-             max: {}s\n
-             min: {}s",
-        times.iter().cloned().fold(0.0, |a, b| { a + b }) / rounds as f64,
-        times.iter().cloned().fold(0. / 0., f64::max),
-        times.iter().cloned().fold(1. / 0., f64::min)
-    );
+fn read_num(default: Option<u32>) -> u32 {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read from stdin");
+
+    match default {
+        Some(default) => input.trim().parse().unwrap_or(default),
+        None => {
+            match input.trim().parse::<u32>() {
+                Ok(val) => val,
+                Err(_)  => read_num(None)
+            }
+        }
+    }
+}
+
+fn mean_f64(list: &Vec<f64>) -> f64 {
+    let sum: f64 = Iterator::sum(list.iter());
+    f64::from(sum) / (list.len() as f64)
+}
+
+fn mean_u32(list: &Vec<u32>) -> f64 {
+    let sum: u32 = Iterator::sum(list.iter());
+    f64::from(sum) / (list.len() as f64)
 }
